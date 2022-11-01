cdf8f9707b718d3c703513b290ac79767f2872ae
Ninou01 <ninofcm20@gmail.com>
2022-11-01 17:26:17 +0100

    adding categories to side bar on phone screen

--- js/sidebar-menu.js
+++ js/sidebar-menu.js
@@ -2,6 +2,10 @@ let menuButton = document.querySelector(".menu button")
 let menuSidebar = document.getElementsByClassName("menu-sidebar")[0]
 let menuOverlay = document.getElementsByClassName("menu-overlay")[0]
 
+window.addEventListener("load", () => {
+    document.body.classList.remove("preload")
+})
+
 menuButton.addEventListener("click", () => {
     menuSidebar.classList.add("visible")
 })
