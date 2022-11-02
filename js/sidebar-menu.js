212d2c8ad02f21035e7bdb152251fdc761ce29fe
Ninou01 <ninofcm20@gmail.com>
2022-11-02 01:23:18 +0100

    adding categorie page

--- js/sidebar-menu.js
+++ js/sidebar-menu.js
@@ -1,6 +1,10 @@
 let menuButton = document.querySelector(".menu button")
 let menuSidebar = document.getElementsByClassName("menu-sidebar")[0]
 let menuOverlay = document.getElementsByClassName("menu-overlay")[0]
+let menuLinks = document.getElementsByClassName("menu-links")[0]
+let dropdown = document.getElementsByClassName("dropdown")[0]
+let select = dropdown.querySelector(".select")
+
 
 window.addEventListener("load", () => {
     document.body.classList.remove("preload")
@@ -12,5 +16,10 @@ menuButton.addEventListener("click", () => {
 
 menuOverlay.addEventListener("click", () => {
     menuSidebar.classList.remove("visible")
+    
+})
 
+select.addEventListener("click", () => {
+    dropdown.classList.toggle("active")
+    menuLinks.classList.toggle("scroll")
 })
