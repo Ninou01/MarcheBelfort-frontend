ccc45c2a87e50302443027d9d21531bfb7fe7aa0
Ninou01 <ninofcm20@gmail.com>
2022-11-06 21:41:39 +0100

    correcting js commande-form errors

--- js/commande-form.js
+++ js/commande-form.js
@@ -1,7 +1,7 @@
 let form = document.getElementsByTagName("form")[0]
 let quantité = form.querySelector('input[type="number"]')
-let price = document.getElementsByClassName("produit-price")[0]
-let totalPrice = document.getElementById("price")
+let price = document.getElementById("price")
+let totalPrice = document.getElementById("total-price")
 let wilayat = document.getElementById("wilaya")
 let communes = document.getElementById("commune")
 let errorsBox = document.querySelector(".errors-box")
@@ -25,7 +25,12 @@ wilayat.addEventListener("change", function () {
     .then((result) => result.json())
     .then(data => {
         let wilayaCode = +wilayat.selectedOptions[0].getAttribute("data-wilaya-code")
+        
+        // sort communes by alphabetic order
         data = data.filter(element => parseInt(element["wilaya_code"]) === wilayaCode)
+        data.sort((a,b) => a.commune_name_ascii < b.commune_name_ascii ? -1 : a.commune_name_ascii < b.commune_name_ascii ? 1 : 0)
+        communes.innerHTML = ""
+
         for (let i = 0; i < data.length; i++) {
             d = data[i]
             let option = document.createElement("option")
@@ -37,12 +42,28 @@ wilayat.addEventListener("change", function () {
 })
 
 totalPrice.textContent = price.textContent
+// changing price by changing quantity with arrows
 quantité.addEventListener("change", () => {
+    totalPrice.textContent = +price.textContent * +quantité.value
+})
+// changing price by changing quantity with keyboard
+quantité.addEventListener("keyup", () => {
     totalPrice.textContent = +price.textContent * +quantité.value
 })
 
 
 form.addEventListener("submit", (e) => {
+    
+    // quantity validator
+    if (quantité.value <= 0) {
+        e.preventDefault()
+        let error = document.createElement("li")
+        error.textContent = "quantité invalide"
+        errorsBox.append(error)
+        errorsBox.classList.add("visible")
+    }
+    
+    // phone number validator
     const reg=  /^(00213|\+213|0)(5|6|7)[0-9]{8}$/
     const phone_number = form.querySelector("#phone_number")
     const phone_number2 = form.querySelector("#phone_number2")
@@ -53,11 +74,13 @@ form.addEventListener("submit", (e) => {
         error.textContent = "numero de telephone invalide"
         errorsBox.append(error)
         errorsBox.classList.add("visible")
-        setTimeout(() => {
-            errorsBox.innerHTML = ""
-            errorsBox.classList.remove("visible")
-            
-          }, "10000")
     }
+
+    // delete error messages after 10s
+    setTimeout(() => {
+        errorsBox.innerHTML = ""
+        errorsBox.classList.remove("visible")
+        
+      }, "10000")
 })
 
